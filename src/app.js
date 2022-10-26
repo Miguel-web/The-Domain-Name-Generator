3891159d33deeaaf5203850c5049eb0a7651d350
Miguel-web <miguelgilf@gmail.com>
2022-10-26 18:18:31 +0000

    firstcommit

--- src/app.js
+++ src/app.js
@@ -4,8 +4,28 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+//import { apply } from "file-loader";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+//write your code here
+
+window.onload = () => {
+  //document.querySelector("#domains").innerHTML = domains();
+  document.querySelector("#btn").addEventListener("click", newDomain);
+  document.querySelector("#domains").innerHTML = domains();
 };
+
+const pronoun = ["the", "our"];
+const adj = ["great", "big"];
+const noun = ["jogger", "racoon"];
+const exten = [".com", ".es", ".io", ".us"];
+
+const randomNumber = max => Math.floor(Math.random() * max);
+const domains = () => `${pronoun[randomNumber(pronoun.length)]} 
+${adj[randomNumber(adj.length)]} 
+${noun[randomNumber(noun.length)]} 
+${exten[randomNumber(exten.length)]}`;
+
+const newDomain = () => {
+  document.querySelector("#domains").innerHTML = domains();
+};
+//   return who[whoIndex] + " " + what[whatIndex] + " " + when[whenIndex];
